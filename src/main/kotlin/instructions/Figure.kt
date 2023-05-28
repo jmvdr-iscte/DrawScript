bdc4f6e20a9b576284b14f0d587cb8f25201c59e
jmvdr-iscte <ze.correia467@gmail.com>
2023-05-28 19:33:58 +0100

    Add rectangle

---
+++ src/main/kotlin/instructions/Figure.kt
@@ -0,0 +1,16 @@
+package instructions
+
+import constants.Localization
+import expressions.Expression
+
+open class Figure(
+    val localization: Localization
+):Instruction
+    class Rectangle(
+        localization: Localization,
+        var width: Expression,
+        var height: Expression
+    ) : Figure(localization){
+        fun getRectangle(){
+        }
+    }
